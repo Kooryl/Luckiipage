31fb9a96ace4fddb8dc3be19d6194e46208c893d
andris-jacob <andris@jacob.world>
2026-03-11 09:46:00 +0100

    StartGameRequest Class

--- src/main/java/dev/zwazel/springintro/games/blackjack/StartGameRequest.java
+++ src/main/java/dev/zwazel/springintro/games/blackjack/StartGameRequest.java
@@ -7,3 +7,21 @@
  * - validate bet > 0
  * - keep request minimal
  */
+public class StartGameRequest {
+    private double bet;
+
+    public StartGameRequest(double bet) {
+        setBet(bet);
+    }
+
+    public double getBet() {
+        return bet;
+    }
+
+    public void setBet(double bet) {
+        if (bet <= 0) {
+            throw new IllegalArgumentException("Bet must be greater than 0");
+        }
+        this.bet = bet;
+    }
+}
